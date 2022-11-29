6e9465fb8af422334c2f0915d68a7e23721c85e3
Socially Awkward Streams <candice.radam@gmail.com>
2022-11-29 15:31:17 -0600

    Tailwind styling.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fs = require("fs");
+const express = require('express');
 const inquirer = require("inquirer");
 
-
+app.listen(3000, function()
+{console.log("Note Taker Application is running on port 3000.")
+});
