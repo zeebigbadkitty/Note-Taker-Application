2d8ec758d2a8bfc0372de46a2d6e48a969d6d1b3
Socially Awkward Streams <candice.radam@gmail.com>
2022-12-01 12:02:33 -0600

    Working on deleting notes.

--- apiroutes.js
+++ apiroutes.js
@@ -10,9 +10,26 @@ res.json(dbstore)
 })
 
 router.post("/notes", (req, res)=>{
-    
+//request from the frontend
+let newNote = {
+   title: req.body.title, 
+   text: req.body.text,
+   id:Math.random()
+}    
+//pushing it
+dbstore.push(newNote)
+//rewriting the array with new note
+fs.writeFileSync("./db/db.json", JSON.stringify(dbstore))
+
+res.json(dbstore)
 })
+//"declaring what we want to keep"
+router.delete("/notes/:id", (req, res)=>{
+let updatedNotes = []
+for(var i = 0; i<dbstore.length; i++) {
+    
+}
 
-
+})
 
 module.exports=router
