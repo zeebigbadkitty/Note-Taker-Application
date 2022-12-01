5dee2b6ba6646631ef49ffcc1e12a2f97ca177e6
Socially Awkward Streams <candice.radam@gmail.com>
2022-12-01 07:03:09 -0600

    I didn't realize there was starter code, so I updated my files with the starter files.

--- index.js
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-const express = require('express');
-const inquirer = require("inquirer");
-
-app.listen(3000, function()
-{console.log("Note Taker Application is running on port 3000.")
-});
